9df72820c6cf46f1562112a7885356fdde824145
nicookazan <kazandjiannicolas@gmail.com>
2026-03-10 21:17:48 -0300

    cambios base de instalación

--- src/application/MainProgram.java
+++ src/application/MainProgram.java
@@ -1,10 +1,10 @@
 package application;
 
 public class MainProgram {
+
     private boolean running = true;
 
     public static void tp1() {
         System.out.println("Hello and welcome!");
-
     }
 }
